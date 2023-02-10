887410aba2714ac6e5f5394ab1faa529e2bf0500
like5188 <260864866@qq.com>
2023-02-10 19:11:36 +0800

    完善

--- socket/src/main/java/com/like/chengdu/socket/client/NettyClient.kt
+++ socket/src/main/java/com/like/chengdu/socket/client/NettyClient.kt
@@ -1,10 +1,7 @@
 package com.like.chengdu.socket.client
 
 import io.netty.bootstrap.Bootstrap
-import io.netty.channel.ChannelFuture
-import io.netty.channel.ChannelFutureListener
-import io.netty.channel.ChannelInitializer
-import io.netty.channel.EventLoopGroup
+import io.netty.channel.*
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioSocketChannel
@@ -13,7 +10,7 @@ import java.util.concurrent.TimeUnit
 /**
  * 实现了重连的客户端
  */
-class NettyClient(private val host: String, private val port: Int) {
+class NettyClient(host: String, port: Int) {
     private val bootstrap: Bootstrap by lazy {
         Bootstrap()
     }
@@ -36,13 +33,16 @@ class NettyClient(private val host: String, private val port: Int) {
     init {
         bootstrap.group(eventLoopGroup)
             .channel(NioSocketChannel::class.java)
+            .remoteAddress(host, port)
+            .option(ChannelOption.TCP_NODELAY, true)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
             .handler(channelInitializer)
     }
 
     fun connect() {
         println("尝试连接服务器...")
         //异步连接服务器端
-        val cf: ChannelFuture = bootstrap.connect(host, port)
+        val cf: ChannelFuture = bootstrap.connect()
         cf.addListener(ChannelFutureListener { future ->
             if (!future.isSuccess) {
                 println("服务端连接失败！")
