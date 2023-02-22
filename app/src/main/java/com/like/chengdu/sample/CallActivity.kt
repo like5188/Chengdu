fdac450dbf2c31261c728f34fceb3d572be3a774
like5188 <260864866@qq.com>
2023-02-22 16:37:16 +0800

    完善

--- app/src/main/java/com/like/chengdu/sample/CallActivity.kt
+++ app/src/main/java/com/like/chengdu/sample/CallActivity.kt
@@ -1,27 +1,19 @@
 package com.like.chengdu.sample
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.database.ContentObserver
 import android.graphics.Color
 import android.os.Bundle
-import android.provider.CallLog
 import android.text.method.ScrollingMovementMethod
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
-import com.like.chengdu.call.*
+import com.like.chengdu.call.AudioUtils
+import com.like.chengdu.call.CallManager
+import com.like.chengdu.call.LocalCall
 import com.like.chengdu.sample.databinding.ActivityCallBinding
-import com.like.common.util.Logger
-import com.like.common.util.activityresultlauncher.requestMultiplePermissions
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-
 
 @SuppressLint("MissingPermission")
 class CallActivity : AppCompatActivity() {
@@ -31,107 +23,33 @@ class CallActivity : AppCompatActivity() {
     private val audioUtils by lazy {
         AudioUtils()
     }
-    private var config: ScanCallRecordingConfig? = null
-    private val callRecordingFileUtils by lazy {
-        CallRecordingFileUtils()
-    }
     private var curLocalCall: LocalCall? = null
+    private val callManager by lazy {
+        CallManager(this) { localCall, uploadFile, uploadLocalCall ->
+            curLocalCall = localCall
+            // 更新ui
+            mBinding.tvCall.text = localCall.toString()
+            if (uploadFile) {
+                mBinding.tvUploadFile.text = "上传录音文件成功!"
+                mBinding.tvUploadFile.setTextColor(Color.parseColor("#00ff00"))
+            } else {
+                mBinding.tvUploadFile.text = "上传录音文件失败!"
+                mBinding.tvUploadFile.setTextColor(Color.parseColor("#ff0000"))
+            }
+            if (uploadLocalCall) {
+                mBinding.tvUploadCall.text = "上传通话记录成功!"
+                mBinding.tvUploadCall.setTextColor(Color.parseColor("#00ff00"))
+            } else {
+                mBinding.tvUploadCall.text = "上传通话记录失败!"
+                mBinding.tvUploadCall.setTextColor(Color.parseColor("#ff0000"))
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding.etMsg.movementMethod = ScrollingMovementMethod.getInstance()
-        lifecycleScope.launch {
-            config = NetApi.getScanCallRecordingConfig("http://47.108.214.93/call.json")?.apply {
-                callRecordingFileUtils.init(this)
-            }
-        }
-        listenPhoneState()
-    }
-
-    private fun listenPhoneState() {
-        lifecycleScope.launch {
-            val requestMultiplePermissions = requestMultiplePermissions(
-                Manifest.permission.READ_PHONE_STATE,
-                Manifest.permission.PROCESS_OUTGOING_CALLS,
-                Manifest.permission.READ_CALL_LOG,
-                Manifest.permission.WRITE_CALL_LOG,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-            ).all { it.value }
-            if (!requestMultiplePermissions) {
-                return@launch
-            }
-
-            PhoneReceiver.listen(
-                this@CallActivity,
-                {
-                    Logger.e("开始嘟嘟嘟……")
-                },
-                {
-                    Logger.e("挂断")
-                    val hungUpTime = System.currentTimeMillis()
-                    lifecycleScope.launch(Dispatchers.IO) {
-                        listenOnceCallLogChange {
-                            // 获取通话记录
-                            val call = CallUtils.getLatestCallByPhoneNumber(this@CallActivity, it)
-                                ?: return@listenOnceCallLogChange
-                            val localCall = LocalCall(call).apply {
-                                this.dateOfCallHungUp = hungUpTime
-                            }
-
-                            // 获取录音文件
-                            val files = callRecordingFileUtils.getCallRecordingFile()
-                            // 转换成wav格式
-                            val file = AudioConverter.convertToWav(files.firstOrNull())
-                            // 上传文件
-                            val uploadResult =
-                                UploadUtils.upload(this@CallActivity, localCall, file)
-                            // 更新ui
-                            withContext(Dispatchers.Main) {
-                                mBinding.tvCall.text = localCall.toString()
-                                if (uploadResult.first) {
-                                    mBinding.tvUploadFile.text = "上传录音文件成功!"
-                                    mBinding.tvUploadFile.setTextColor(Color.parseColor("#00ff00"))
-                                } else {
-                                    mBinding.tvUploadFile.text = "上传录音文件失败!"
-                                    mBinding.tvUploadFile.setTextColor(Color.parseColor("#ff0000"))
-                                }
-                                if (uploadResult.second) {
-                                    mBinding.tvUploadCall.text = "上传通话记录成功!"
-                                    mBinding.tvUploadCall.setTextColor(Color.parseColor("#00ff00"))
-                                } else {
-                                    mBinding.tvUploadCall.text = "上传通话记录失败!"
-                                    mBinding.tvUploadCall.setTextColor(Color.parseColor("#ff0000"))
-                                }
-                            }
-                            curLocalCall = localCall
-                        }
-                    }
-                }
-            )
-        }
-    }
-
-    /**
-     * 监听一次系统通话记录数据库的改变
-     */
-    private suspend fun listenOnceCallLogChange(onChanged: suspend () -> Unit) {
-        val stateFlow = MutableStateFlow(false)
-        val callLogObserver = object : ContentObserver(null) {
-            override fun onChange(selfChange: Boolean) {
-                super.onChange(selfChange)
-                stateFlow.value = selfChange
-            }
-        }
-        contentResolver.registerContentObserver(
-            CallLog.Calls.CONTENT_URI,
-            true,
-            callLogObserver
-        )
-        stateFlow.debounce(500).collectLatest {
-            contentResolver.unregisterContentObserver(callLogObserver)
-            onChanged.invoke()
-        }
+        callManager
     }
 
     fun call(view: View) {
@@ -139,22 +57,11 @@ class CallActivity : AppCompatActivity() {
         if (phone.isNullOrEmpty()) {
             return
         }
-        lifecycleScope.launch {
-            val requestMultiplePermissions = requestMultiplePermissions(
-                Manifest.permission.CALL_PHONE,
-            ).all { it.value }
-            if (!requestMultiplePermissions) {
-                return@launch
-            }
-            curLocalCall = null
-            withContext(Dispatchers.Main) {
-                mBinding.tvCall.text = ""
-                mBinding.tvUploadCall.text = ""
-                mBinding.tvUploadFile.text = ""
-            }
-            callRecordingFileUtils.startWatching()
-            CallUtils.call(this@CallActivity, phone)
-        }
+        curLocalCall = null
+        mBinding.tvCall.text = ""
+        mBinding.tvUploadCall.text = ""
+        mBinding.tvUploadFile.text = ""
+        callManager.call(phone)
     }
 
     fun start(view: View) {
@@ -179,7 +86,7 @@ class CallActivity : AppCompatActivity() {
     fun getCalls(view: View) {
         mBinding.etMsg.setText("")
         lifecycleScope.launch(Dispatchers.Main) {
-            CallUtils.getLatestCalls(this@CallActivity, 10).forEach {
+            callManager.getLatestCalls(10).forEach {
                 val oldMsg = mBinding.etMsg.text?.toString()
                 val text = if (oldMsg.isNullOrEmpty()) {
                     it.toString()
---
+++ call/src/main/java/com/like/chengdu/call/CallManager.kt
@@ -0,0 +1,116 @@
+package com.like.chengdu.call
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.database.ContentObserver
+import android.provider.CallLog
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
+import com.like.common.util.activityresultlauncher.requestMultiplePermissions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@SuppressLint("MissingPermission")
+class CallManager(
+    private val activity: ComponentActivity,
+    private val uploadResult: (LocalCall, uploadFile: Boolean, uploadLocalCall: Boolean) -> Unit
+) {
+    private val callRecordingFileUtils by lazy {
+        CallRecordingFileUtils()
+    }
+
+    init {
+        activity.lifecycleScope.launch {
+            val requestMultiplePermissions = activity.requestMultiplePermissions(
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.PROCESS_OUTGOING_CALLS,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.WRITE_CALL_LOG,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CALL_PHONE,
+            ).all { it.value }
+            if (!requestMultiplePermissions) {
+                return@launch
+            }
+            NetApi.getScanCallRecordingConfig("http://47.108.214.93/call.json")?.apply {
+                callRecordingFileUtils.init(this)
+            }
+            listenPhoneState()
+        }
+    }
+
+    private suspend fun listenPhoneState() {
+        PhoneReceiver.listen(
+            activity,
+            {
+                println("开始嘟嘟嘟……")
+            },
+            {
+                println("挂断")
+                val hungUpTime = System.currentTimeMillis()
+                activity.lifecycleScope.launch(Dispatchers.IO) {
+                    listenOnceCallLogChange {
+                        // 获取通话记录
+                        val call = CallUtils.getLatestCallByPhoneNumber(activity, it)
+                            ?: return@listenOnceCallLogChange
+                        val localCall = LocalCall(call).apply {
+                            this.dateOfCallHungUp = hungUpTime
+                        }
+
+                        // 获取录音文件
+                        val files = callRecordingFileUtils.getCallRecordingFile()
+                        // 转换成wav格式
+                        val file = AudioConverter.convertToWav(files.firstOrNull())
+                        // 上传
+                        val uploadResult = UploadUtils.upload(activity, localCall, file)
+                        withContext(Dispatchers.Main) {
+                            uploadResult(localCall, uploadResult.first, uploadResult.second)
+                        }
+                    }
+                }
+            }
+        )
+    }
+
+    /**
+     * 监听一次系统通话记录数据库的改变
+     */
+    private suspend fun listenOnceCallLogChange(onChanged: suspend () -> Unit) {
+        val stateFlow = MutableStateFlow(false)
+        val callLogObserver = object : ContentObserver(null) {
+            override fun onChange(selfChange: Boolean) {
+                super.onChange(selfChange)
+                stateFlow.value = selfChange
+            }
+        }
+        activity.contentResolver.registerContentObserver(
+            CallLog.Calls.CONTENT_URI,
+            true,
+            callLogObserver
+        )
+        stateFlow.debounce(500).collectLatest {
+            activity.contentResolver.unregisterContentObserver(callLogObserver)
+            onChanged.invoke()
+        }
+    }
+
+    fun call(phone: String?) {
+        if (phone.isNullOrEmpty()) {
+            return
+        }
+        activity.lifecycleScope.launch {
+            callRecordingFileUtils.startWatching()
+            CallUtils.call(activity, phone)
+        }
+    }
+
+    suspend fun getLatestCalls(num: Int): List<Call> {
+        return CallUtils.getLatestCalls(activity, num)
+    }
+
+}
